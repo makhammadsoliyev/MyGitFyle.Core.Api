755e7cb7db08235b4e2adc9fde79ea9bc15fa5a8
Umidjon <makhammadsoliyev@gmail.com>
2024-07-07 13:35:07 +0500

    INFRA: Build Script

---
+++ GitFyle.Core.Infrastructure/Program.cs
@@ -0,0 +1,10 @@
+﻿using GitFyle.Core.Infrastructure.Services;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var generateScriptservice = new ScriptGenerationService();
+        generateScriptservice.GenerateBuildScript();
+    }
+}
